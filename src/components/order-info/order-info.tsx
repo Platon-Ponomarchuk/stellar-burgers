836661190f34552ceba2da4a5aac06c72b16361d
Platon Ponomarchuk <ponomarchukdevelop@gmail.com>
2024-10-31 00:29:04 +0300

    Добавлена история заказов

--- src/components/order-info/order-info.tsx
+++ src/components/order-info/order-info.tsx
@@ -1,23 +1,33 @@
-import { FC, useMemo } from 'react';
+import { FC, useEffect, useMemo } from 'react';
 import { Preloader } from '../ui/preloader';
 import { OrderInfoUI } from '../ui/order-info';
 import { TIngredient } from '@utils-types';
+import { useDispatch, useSelector } from '../../services/store';
+import { selectIngredients } from '../../services/Slices/IngredientsSlice';
+import {
+  clearOrder,
+  fetchOrderById,
+  selectCurrentOrder
+} from '../../services/Slices/OrderSlice';
+import { useParams } from 'react-router-dom';
 
 export const OrderInfo: FC = () => {
-  /** TODO: взять переменные orderData и ingredients из стора */
-  const orderData = {
-    createdAt: '',
-    ingredients: [],
-    _id: '',
-    status: '',
-    name: '',
-    updatedAt: 'string',
-    number: 0
-  };
+  const dispatch = useDispatch();
 
-  const ingredients: TIngredient[] = [];
+  const orderData = useSelector(selectCurrentOrder);
 
-  /* Готовим данные для отображения */
+  const ingredients: TIngredient[] = useSelector(selectIngredients);
+
+  const param = useParams<{ number: string }>().number;
+
+  useEffect(() => {
+    dispatch(fetchOrderById(parseInt(param || '') || -1));
+
+    return () => {
+      dispatch(clearOrder());
+    };
+  }, [dispatch, param]);
+
   const orderInfo = useMemo(() => {
     if (!orderData || !ingredients.length) return null;
 
